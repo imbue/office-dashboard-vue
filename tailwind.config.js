7b89fe8f42d56bb4bfe16f452710d0e7878b62a3
Liam Boer <liamboer@outlook.com>
2019-12-12 22:39:12 +0100

    Apply body padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,26 @@ module.exports = {
     backgroundColor: theme => ({
       screen: 'var(--bg-screen)',
       tile: 'var(--bg-tile)'
+    }),
+    padding: theme => ({
+      px: '1px',
+      '0': '0',
+      '1': '0.25rem',
+      '2': '0.5rem',
+      '3': '0.75rem',
+      '4': '1rem',
+      '5': '1.25rem',
+      '6': '1.5rem',
+      '8': '2rem',
+      '10': '2.5rem',
+      '12': '3rem',
+      '16': '4rem',
+      '20': '5rem',
+      '24': '6rem',
+      '32': '8rem',
+      spacing: 'var(--tile-spacing)',
+      padding: 'var(--tile-padding)'
     })
-    // fontSize: theme => ({
-    //   'base': '1rem'
-    // })
   },
   variants: {},
   plugins: []
